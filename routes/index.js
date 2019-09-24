8e0c9d7b7d12b5b66cfda4d7666a1bbc28ae318e
Eddy Brown <eddybrown@me.com>
2019-09-24 11:51:04 +0100

    More functionality
    Website now has a form. There is some basic verification. We are using Pug as the templater.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,35 @@ const express = require('express');
 
 const router = express.Router();
 
+const { body, validationResult } = require('express-validator/check');
+
 router.get('/', (req, res) => {
-	res.send('It works!');
+	res.render('form', { title: 'Registration form' });
 });
+
+router.post(
+	'/',
+	[
+		body('name')
+			.isLength({ min: 1 })
+			.withMessage('Please enter a name'),
+		body('email')
+			.isLength({ min:1 })
+			.withMessage('Please enter an email'),
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+
+		if (errors.isEmpty()) {
+			res.send('Thank you for your Registration!');
+		} else {
+			res.render('form', {
+				title: 'Registration form',
+				errors: errors.array(),
+				data: req.body,
+			});
+		}
+	}
+);
 
 module.exports = router;
